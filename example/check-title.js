a3aeae7434ecc2e0134fde08c447d151e92031f2
Amit Upadhyay <comments@amitu.com>
2014-01-02 19:59:05 +0530

    demonstrating how to call callback properly

--- example/check-title.js
+++ example/check-title.js
@@ -2,6 +2,14 @@ module.exports = function helloBrowserStack(browser, next) {
     /*
 	next must be called by this method somewhere. 
 	in case test fails next can be passed some error message.
+	
+	eg:
+	
+	if (title === 'Google') {
+      	     next(null);
+        } else {
+            next(new Error('UH OH Title is not ok! Are you on google.com?'));
+        }
     */
 
     browser.title(function(err, title) {
@@ -10,7 +18,7 @@ module.exports = function helloBrowserStack(browser, next) {
             el.sendKeys("BrowserStack", function() {
                 browser.elementByName("btnG", function(err,el) {
                     el.click(function() {
-			next();
+                        next(null);
                     })
                 })
             })
