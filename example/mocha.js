ef2ac37ac40e2cb37613be5d36155a7c17097078
stephen wille <stephen.home@ubu.com>
2015-06-27 18:38:26 -0700

    mocha test: login to ars.

--- example/mocha.js
+++ example/mocha.js
@@ -1,50 +1,59 @@
 var assert = require('assert'),
-  fs = require('fs');
+  fs = require('fs'),
+  browsers = require('./browsers.json');
+
 
 var webdriver = require('browserstack-webdriver'),
-test = require('browserstack-webdriver/testing');
+  test = require('browserstack-webdriver/testing');
 
-test.describe('ars login', function() {
+test.describe('ars login', function () {
   var driver, server;
 
-  test.before(function() {
+  test.before(function () {
+    /* loop through browers.json and set 'capabilities' props.  */
     var capabilities = {
-      'browserName' : 'firefox',
-      'browserstack.user' : 'stephenwille1',
-      'browserstack.key' : 'CK6vaR6UxtZnk5MnhhMo'
-    }
+      'browserName': 'firefox',
+      'browserstack.user': 'stephenwille1',
+      'browserstack.key': 'CK6vaR6UxtZnk5MnhhMo'
+    };
+
+
     driver = new webdriver.Builder().
-      usingServer('http://localhost:4444/wd/hub').
+      usingServer('http://hub.browserstack.com/wd/hub').
+      //usingServer('http://localhost:4444/wd/hub').
       withCapabilities(capabilities).
       build();
   });
 
-  test.it('should log into ars technica', function() {
+
+
+  test.it('should go to ars technica', function () {
     driver.get('http://arstechnica.com/').then(function () {
       driver.sleep(100);
     });
   });
-  test.it('login', function () {
+
+
+  test.it('should login', function () {
     driver.findElement(webdriver.By.css('#login')).click().then(function () {
       driver.sleep(100);
     });
     driver.findElement(webdriver.By.css('#username')).sendKeys('stephendev');
     driver.findElement(webdriver.By.css('#password')).sendKeys('woot-woot');
-    driver.wait(function() {
-      return driver.getTitle().then(function(title) {
-        return 'BrowserStack - Google Search' === title;
-      });
-    }, 1000);
+    driver.findElement(webdriver.By.name('login')).click();
+    driver.findElement(webdriver.By.css('.welcome')).getText().then(function (username) {
+      assert.equal(username, 'stephendev');
+    });
   });
 
 
-  test.after(function() { driver.quit(); });
+  test.after(function () { driver.quit(); });
 });
 
 /*
-install protractor
+ install protractor
  npm install -g protractor
  webdriver-manager update
  webdriver-manager start
-npm install -g browserstack-webdriver
-mocha mocha.js */
+ npm install -g browserstack-webdriver
+ mocha mocha.js */
